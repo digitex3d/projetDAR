84a569273bce5f1625e43b1112b8ad72677badb6
digitex3d <digitex3d@gmail.com>
2016-10-01 16:30:24 +0200

    Meilleur logging

--- src/database/DBStatic.java
+++ src/database/DBStatic.java
@@ -3,6 +3,7 @@ import java.net.UnknownHostException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
 
 import services.service.GeneralTools;
 
@@ -17,21 +18,38 @@ import com.mongodb.MongoException;
 public class DBStatic {
 	
 
-	private static final String DB_NAME = System.getenv("OPENSHIFT_APP_NAME");
-	private static final String DB_HOST = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
-	private static final String DB_PORT = System.getenv("OPENSHIFT_MYSQL_DB_PORT");
-	private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
+//	private static final String DB_NAME = System.getenv("OPENSHIFT_APP_NAME");
+//	private static final String DB_HOST = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
+//	private static final String DB_PORT = System.getenv("OPENSHIFT_MYSQL_DB_PORT");
+//	private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
+//	
+//	private static final String DB_CONNECTION = "jdbc:mysql://"+ DB_HOST+":"+DB_PORT +"/" + DB_NAME;
+//	private static final String DB_USER = System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+//	private static final String DB_PASSWORD = System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
+//	private static final String MONGO_ADR = System.getenv("OPENSHIFT_MONGODB_DB_HOST");
+//	private static final Integer MONGO_PORT = Integer.decode(System.getenv("OPENSHIFT_MONGODB_DB_PORT"));
 	
+	// Configuration à utiliser que en LOCALE 
+	
+	//  ################## [ conf locale   
+	
+	private static final String DB_NAME = "dar";
+	private static final String DB_HOST = "127.0.0.1";
+	private static final String DB_PORT = "3306";
+	private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
 	private static final String DB_CONNECTION = "jdbc:mysql://"+ DB_HOST+":"+DB_PORT +"/" + DB_NAME;
-	private static final String DB_USER = System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
-	private static final String DB_PASSWORD = System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
-	private static final String MONGO_ADR = System.getenv("OPENSHIFT_MONGODB_DB_HOST");
-	private static final Integer MONGO_PORT = Integer.decode(System.getenv("OPENSHIFT_MONGODB_DB_PORT"));
+	private static final String DB_USER = "dar";
+	private static final String DB_PASSWORD = "dar";
+	private static final String MONGO_ADR = "127.0.0.1";
+	private static final Integer MONGO_PORT = 27017;
+	
+	//   					 conf locale ] ##################
 	
 	/*
 	 * Retourne une connexion Mongo
 	 */
 	public static Mongo getMongoConnection() throws UnknownHostException, MongoException{
+
 		System.out.println("-------- MONGO Connection Testing ------------\n");
 		
 		Mongo m = new Mongo(MONGO_ADR, MONGO_PORT);	
@@ -44,7 +62,9 @@ public class DBStatic {
 	 * Retourne une connexion à MySQL
 	 */
 	public static Connection getMySQLConnection() throws SQLException{
-		System.out.println("-------- MySQL JDBC Connection Testing ------------\n");
+		
+		GeneralTools.serverLog("-------- MySQL JDBC Connection Testing ------------\n", "DBStatic", Level.FINER);
+
 		 
 		try {
 			Class.forName(DB_DRIVER);
@@ -55,18 +75,18 @@ public class DBStatic {
 		
 		}
 	 
-		System.out.println("MySQL JDBC Driver enregistré");
+		GeneralTools.serverLog("MySQL JDBC Driver enregistré\n", "DBStatic", Level.FINER);
+
 		Connection connection = null;
-	 
-		
+	 	
 		connection = DriverManager.getConnection(DB_CONNECTION,DB_USER , DB_PASSWORD);
 	 
 	 
 		if (connection != null) {
-			GeneralTools.serverLog("Connexion à MySQL réussie!");
+			GeneralTools.serverLog("Connexion à MySQL réussie!",  Level.FINE);
 			
 		} else {
-			GeneralTools.serverLog("Pas possible de se connecter à MySQL!");
+			GeneralTools.serverLog("Pas possible de se connecter à MySQL!",  Level.FINE);
 			
 		}
 		
